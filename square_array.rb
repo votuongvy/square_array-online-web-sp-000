78ded8add7a35701cc06ed59b2eac2bb728f11ba
vy vo <tuonggvy.vo@gmail.com>
2019-10-30 04:33:10 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   # your code here
   new_array = []
   array.each do |number|
-    new_array.push(number*number)
+    new_array.push(number**2)
     return new_array
   end
 end
